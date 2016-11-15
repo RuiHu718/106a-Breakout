72a08c5fadacce07c9b2c741f662bd874872fc74
S <s>
2016-11-15 08:51:35 +0800

    {"types":"C","runs":58}

--- Breakout.java
+++ Breakout.java
@@ -72,7 +72,7 @@ public class Breakout extends GraphicsProgram {
 
     private void setUpBricks(){
     	//setUpOneRow(70, Color.RED);
-    	setUpOneRow(70+BRICK_HEIGHT, Color.RED);
+    	//setUpOneRow(70+BRICK_HEIGHT, Color.RED);
     	setUpOneRow(70+2*BRICK_HEIGHT, Color.RED);
     	setUpOneRow(70+3*BRICK_HEIGHT, Color.BLUE);
     	setUpOneRow(70+4*BRICK_HEIGHT, Color.RED);
